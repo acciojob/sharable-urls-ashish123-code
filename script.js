c7272f7f76c0103b71748df755eb5a125962741b
ashish sahoo <119804958+ashish123-code@users.noreply.github.com>
2023-02-15 10:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+function handelSubmit(event){
+          event.preventDefault();
+          var h3Value="https://localhost:8080/";
+          var name=nameElement.value;
+          var year =yearElement.value;
+
+          if(name && year){
+            h3Value+="?name=" +name+"&year="+year;
+          }else if(name && !year){
+            h3Value+="?name="+name;
+          }else if(!name && year){
+            h3Value+="?year="+year;
+          }
+          h3Value.textContent=h3Value;
+        }
